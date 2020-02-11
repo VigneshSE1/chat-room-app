eb9eb099f71882d97075e17591432b9b88d7e76a
VigneshSE1 <48944738+VigneshSE1@users.noreply.github.com>
2020-02-11 11:15:51 +0530

    ui created

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,31 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { RegisterComponent } from './register/register.component';
+import { LoginComponent } from './login/login.component';
+import { ChatRoomComponent } from './chat-room/chat-room.component'
+import { from } from 'rxjs';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: "",
+    component: HomeComponent
+  },
+  {
+    path: "register",
+    component: RegisterComponent,
+
+  },
+  {
+    path: "login",
+    component: LoginComponent,
+  },
+  {
+    path: "chatroom",
+    component: ChatRoomComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { RegisterComponent } from './register/register.component';
+import { LoginComponent } from './login/login.component';
+import { ChatRoomComponent } from './chat-room/chat-room.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RegisterComponent,
+    LoginComponent,
+    ChatRoomComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
